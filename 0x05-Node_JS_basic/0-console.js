0eda567dd2cea25aa86aeff50d546d1dddf443f3
Therese-Claire <awonguagabi@gmail.com>
2024-08-20 23:27:26 +0000

    exec basic js with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
-exports.displayMessage = function(message) {
-	console.log(message);
+/**
+ * Prints the given message to STDOUT.
+ * 
+ * @param {msg} str - The message to be printed.
+ */
+function displayMessage(message) {
+  console.log(message);
 }
+
+module.export = displayMessage;
